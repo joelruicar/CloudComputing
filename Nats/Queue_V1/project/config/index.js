a2612113b015e180df7653de2c90389a7eb500fe
juanCCloud <154241117+juanCCloud@users.noreply.github.com>
2024-01-19 17:39:54 +0100

    Organizacion del repositorio.

--- Nats/Queue_V1/project/config/index.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  port: process.env.PORT,
-  redis: {
-    port: process.env.REDIS_PORT,
-    host: process.env.REDIS_HOST,
-    db: process.env.REDIS_DB,
-  },
-};
--- Nats/Queue_V1/project/workers/appIn.js
+++
@@ -1,8 +0,0 @@
-module.exports = (job,done) => {
-    try {
-        job.progress(100);
-        done(null,job.data);
-    } catch (error) {
-        done(error);
-    }
-};
