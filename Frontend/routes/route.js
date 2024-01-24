c0bf4bbb182b35a7272c0c72804d6b4e135b6e36
unknown <lizet.chamorro@gmail.com>
2024-01-24 18:13:04 +0100

    KVstorage

--- Frontend/routes/route.js
+++ Frontend/routes/route.js
@@ -1,5 +1,57 @@
 const express = require("express");
 const { connect } = require('nats');
+const {createWork,exportKVstore } = require('../queue.js');
+const routeApi = express.Router();  //creacion de variable para acceder a los metodos de Router de express
+
+routeApi.post('/job', async (req, res) => {
+    const job = req.body;
+
+    createWork(job).then((data) => {
+        res.json({jobId:data});
+    })
+    .catch((error) => {
+        res.status(500).json({error:error,message});
+    });
+
+});
+
+routeApi.post('/job/queue', async (req, res) => {
+    const job = req.body;
+
+    createKVstore(job).then((data) => {
+        res.json({jobId:data});
+    })
+    .catch((error) => {
+        res.status(500).json({error:error,message});
+    });
+
+});
+
+//Recuperar los ID enviados
+routeApi.get('/job/queue/out', async (req, res) => {
+    const job = req.body;
+
+    exportKVstore(job).then((data) => {
+        res.json({jobId:data});
+    })
+    .catch((error) => {
+        res.status(500).json({error:error,message});
+    });
+
+});
+
+
+
+module.exports = routeApi;
+
+
+
+
+
+
+
+/* const express = require("express");
+const { connect } = require('nats');
 const {createWork} = require('../queue.js');
 const routeApi = express.Router();  //creacion de variable para acceder a los metodos de Router de express
 
@@ -16,3 +68,4 @@ routeApi.post('/job', async (req, res) => {
 });
 
 module.exports = routeApi;
+ */
