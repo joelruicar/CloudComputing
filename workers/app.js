9425089dcff8d6f45bc922124790d5a4420ca187
Joel <joel.ruiz2180@gmail.com>
2024-01-26 21:51:17 +0100

    Conexion kv y worker en proceso

--- workers/app.js
+++ workers/app.js
@@ -8,11 +8,12 @@ const destinationFolder = 'temp'
 const rimraf = require('rimraf');
 const util = require('util');
 const execAsync = util.promisify(require('child_process').exec);
+const { ReadableStream } = require('node:stream/web');
 
-async function buscarDatos(id, kv,sc) {
-  rimraf.sync(destinationFolder);
+async function buscarDatos(id, kv,sc, os) {
+  // rimraf.sync(destinationFolder);
+
   const data = await kv.get(id);
-
   if (data) {
     keyValueChanges(id, kv, sc, "executing")
     try {
@@ -22,37 +23,26 @@ async function buscarDatos(id, kv,sc) {
       await execAsync(`git clone ${url} ${destinationFolder}`);
     } catch (error) {
       console.error(`Error: ${error.message}`);
-      saveResultToFile(id, error.message)
       keyValueChanges(id, kv, sc, "error")
     }
   }
   else {
       console.log("ID inválido")
+      await guardarEnOS(id, os, sc, "ID inválido")
+      keyValueChanges(id, kv, sc, "error")
   }
 }
 
 async function keyValueChanges(id, kv, sc, estadoParam) {
   let data = await kv.get(id);
-  let existingState = JSON.parse(sc.decode(data.value))
-  console.log(existingState)
-  existingState.STATE = estadoParam
-  finalState = JSON.stringify(existingState)
-  await kv.put(id, sc.encode(finalState))
-  console.log(finalState)
-}
-
-function saveResultToFile(id, data) {
-  const resultFilePath = path.join('results', `${id}.stdoud`);
-
-  fs.writeFile(resultFilePath, data, (err) => {
-    if (err) {
-      console.error(`Error al guardar el resultado en el archivo: ${err}`);
-    } else {
-      console.log(`Resultado guardado en el archivo: ${resultFilePath}`);
-    }
-  });
-
-  
+  console.log(data, id , "aqui")
+  if (data) {
+    let existingState = JSON.parse(sc.decode(data))
+    existingState.STATE = estadoParam
+    finalState = JSON.stringify(existingState)
+    await kv.put(id, sc.encode(finalState))
+  }
+ 
 }
 
 function obtenerExtension() {
@@ -67,23 +57,24 @@ function obtenerExtension() {
   return extensionMain
 }
 
-async function ejecutarScriptSegunExtension(extension, file, id, kv, sc) {
+async function ejecutarScriptSegunExtension(extension, file, id, kv, sc, os, nc) {
   switch (extension) {
     case 'py':
-      return ejecutarPython(file + "." + extension, id, kv, sc);
+      return ejecutarPython(file + "." + extension, id, kv, sc,os, nc);
     case 'cpp':
-      return ejecutarCPP(file+"."+extension, id, kv, sc);
+      return ejecutarCPP(file+"."+extension, id, kv, sc,os, nc);
     case 'c':
-    return ejecutarC(file+"."+extension, id, kv, sc);
+    return ejecutarC(file+"."+extension, id, kv, sc,os, nc);
       break;
     default:
       console.log(`Extensión no soportada: ${extension}`);
-      saveResultToFile(id, `Extensión no soportada: ${extension}`)
+      await guardarEnOS(id, os, sc, `Extensión no soportada: ${extension}`)
+      keyValueChanges(id, kv, sc, "error")
       return Promise.resolve(null);
   }
 }
 
-async function ejecutarPython(scriptPath, id, kv, sc) {
+async function ejecutarPython(scriptPath, id, kv, sc,os, nc) {
   return new Promise(async (resolve, reject) => {
     try {
       const comando = `python3 ${path.join(destinationFolder, scriptPath)}`;
@@ -91,25 +82,31 @@ async function ejecutarPython(scriptPath, id, kv, sc) {
 
       if (stderr) {
         console.error(`Error al ejecutar el script de Python: ${stderr}`);
+        await guardarEnOS(id, os, sc, stderr)
+        keyValueChanges(id, kv, sc, "error")
         reject(new Error(`Error al ejecutar el script de Python: ${stderr}`));
       } else {
         console.log(`Script de Python ejecutado correctamente: ${stdout}`);
         
-        saveResultToFile(id, stdout);
-        await guardarEnKV(id, kv, sc, stdout)
-        
+        await guardarEnOS(id, os, sc, stdout)
+        keyValueChanges(id, kv, sc, "done")
+         // const natsQueue = 'jobs_Out';
+         nc.publish(natsQueue, sc.encode(JSON.stringify({
+          id,
+          result: stdout,
+        })));
         resolve(stdout);
-
       }
     } catch (error) {
       console.error(`Error al ejecutar el script de Python: ${error.message}`);
-      saveResultToFile(id, stderr)
+      await guardarEnOS(id, os, sc, error)
+      keyValueChanges(id, kv, sc, "error")
       reject(error);
     }
   });
 }
 
-async function ejecutarCPP(scriptPath, id, kv, sc) {
+async function ejecutarCPP(scriptPath, id, kv, sc,os, nc) {
   return new Promise(async (resolve, reject) => {
     try {
       const comando = `g++ ${path.join(destinationFolder, scriptPath)} -o ${path.join(destinationFolder, 'output')}`;
@@ -124,22 +121,31 @@ async function ejecutarCPP(scriptPath, id, kv, sc) {
 
       if (stderr) {
         console.error(`Error al ejecutar el código C++: ${stderr}`);
+        await guardarEnOS(id, os, sc, stderr)
+        keyValueChanges(id, kv, sc, "error")
         reject(new Error(`Error al ejecutar el código C++: ${stderr}`));
       } else {
         console.log(`Código C++ ejecutado correctamente: ${stdout}`);  
-        saveResultToFile(id, stdout)
-        await guardarEnKV(id, kv, sc, stdout)
+        
+        await guardarEnOS(id, os, sc, stdout)
+        keyValueChanges(id, kv, sc, "done")
+        // const natsQueue = 'jobs_Out';
+        nc.publish(natsQueue, sc.encode(JSON.stringify({
+          id,
+          result: stdout,
+        })));
         resolve(stdout);
       }
     } catch (error) {
       console.error(`Error al ejecutar el código C++: ${error.message}`);
-      saveResultToFile(id, error.message);
+      await guardarEnOS(id, os, sc, error.message)
+      keyValueChanges(id, kv, sc, "error")
       reject(error);
     }
   });
 }
 
-async function ejecutarC(scriptPath, id, kv, sc) {
+async function ejecutarC(scriptPath, id, kv, sc, os, nc) {
   return new Promise(async (resolve, reject) => {
     try {
       const ejecutablePath = path.join(destinationFolder, 'output');
@@ -160,29 +166,56 @@ async function ejecutarC(scriptPath, id, kv, sc) {
         reject(new Error(`Error al ejecutar el código C: ${stderr}`));
       } else {
         console.log(`Código C ejecutado correctamente: ${stdout}`);
-        saveResultToFile(id, stdout)
-        await guardarEnKV(id, kv, sc, stdout)
+        await guardarEnOS(id, os, sc, stdout)
+        keyValueChanges(id, kv, sc, "done")
+         // const natsQueue = 'jobs_Out';
+         nc.publish(natsQueue, sc.encode(JSON.stringify({
+          id,
+          result: stdout,
+        })));
+
         resolve(stdout);
       }
     } catch (error) {
       console.error(`Error al ejecutar el código C: ${error.message}`);
-      saveResultToFile(id, error.message);
       reject(error);
     }
   });
 }
 
+function readableStreamFrom(data) {
+  return new ReadableStream({
+    start(controller) {
+      controller.enqueue(data);
+      controller.close();
+    },
+  });
+}
 
- async function guardarEnKV(id, kv, sc, stdout) {
+async function fromReadableStream(rs, sc) {
+  result = ""
+  const reader = rs.getReader();
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
+    if (value && value.length) {
+      result += sc.decode(value);
+    }
+  }
+  return result;
+}
+
+async function guardarEnOS(id, os, sc, stdout) {
   try {
-    let estado = await kv.get(id);
-    const dataJSON = JSON.parse(sc.decode(estado.value))
-    dataJSON.RESULTS = stdout
-    finalState = JSON.stringify(dataJSON)
-    await kv.put(id, sc.encode(finalState))
-    
+    await os.put({
+      name: id,
+    }, readableStreamFrom(sc.encode(JSON.stringify(stdout))));
+  const estado = await os.get(id);
+  result = await fromReadableStream(estado.data, sc);
   } catch (error) {
-    console.error(`Error al leer o guardar el archivo: ${error.message}`);
+    console.error(`Error al leer o guardar el archivo OS: ${error.message}`);
   }
 }
 
@@ -190,52 +223,30 @@ async function run() {
   const sc = StringCodec();  
   //la tarea ya viene on queue
   //Ajustar la URL a la configuracion del NATS mediante docker inspect nats-q
-  const natsUrl = 'nats://172.17.0.2:4222';
-  const natsKVUrl = 'nats://172.17.0.3:4223';
+  //crear cola para enviar resultados, ya no se guardan en KV
+  const natsUrl = '192.168.1.5';
   
-  const nc = await connect({ servers: [natsUrl] });
-  const nckv = await connect({ servers: [natsKVUrl] });
+  const nc = await connect({ servers: natsUrl });
+  const js = await nc.jetstream();
+  const kv = await js.views.kv('jobs_In');
+
   groupName  = 'group1'
-  const sub = nc.subscribe('jobs_executors', {queue: groupName });
+  const sub = nc.subscribe('cola', {queue: groupName });
+  // const res = nc.('jobs_out', {queue: groupName });
  
-  const js = await nckv.jetstream();
-  const kv = await js.views.kv('testing', { history: 5 });
-  const os = await js.views.os("testing", { storage: StorageType.File });
-  
-  //Le pasamos un ID de prueba, suponemos que le llega un string de un JSON
-  if (kv.get("4558308c-4cb2-4194-a8c0-9f5539178a5e") != null) {
-    await kv.delete("4558308c-4cb2-4194-a8c0-9f5539178a5e")
-    await kv.create("4558308c-4cb2-4194-a8c0-9f5539178a5e", sc.encode(JSON.stringify({
-      "URL": "https://github.com/joelruicar/basicPython",
-      "STATE": "in_queue",
-      "RESULTS": ""
-    })))
-  }
-
-  if (kv.get("5558308c-4cb2-4194-a8c0-9f5539178a5e") != null) {
-    await kv.delete("5558308c-4cb2-4194-a8c0-9f5539178a5e")
-    await kv.create("5558308c-4cb2-4194-a8c0-9f5539178a5e", sc.encode(JSON.stringify({
-      "URL": "https://github.com/joelruicar/basicCPP",
-      "STATE": "in_queue",
-      "RESULTS": ""
-    })))
-  }
-
+  const os = await js.views.os("jobs_Out", { storage: StorageType.File });
+ 
   (async () => {
     for await (const m of sub) {
-      let id= sc.decode(m.data)
-      await buscarDatos(id, kv, sc)
+      let id= JSON.parse(sc.decode(m.data)).id
+      await buscarDatos(id, kv, sc, os)
       extensionMain = obtenerExtension()
-      await ejecutarScriptSegunExtension(extensionMain ,"main", id, kv, sc);
-      keyValueChanges(id, kv, sc, "done")
+      await ejecutarScriptSegunExtension(extensionMain ,"main", id, kv, sc,os);
     }
   })();
-  
-  //lo que en teoria le llega al worker 
-  await nc.publish("jobs_executors", sc.encode("4558308c-4cb2-4194-a8c0-9f5539178a5e"))
-  await nc.publish("jobs_executors", sc.encode("5558308c-4cb2-4194-a8c0-9f5539178a5e"))
 }
 
 run().catch((err) => {
   console.error(err);
 });
+
