c98ccec74bb71f85e8dbe5547cfecca3db5145bb
Joel <joel.ruiz2180@gmail.com>
2024-01-30 20:58:13 +0100

    Limpieza de codigo en worker

--- Workers/app.js
+++ Workers/app.js
@@ -31,7 +31,7 @@ async function buscarDatos(id, kv, sc, os) {
   }
   else {
     console.log("ID inválido")
-    await guardarEnOS(id, os, sc, "ID inválido")
+    await guardarEnOS(id, os, sc, "0s", "ID inválido");
     await keyValueChanges(id, kv, sc, "error")
   }
 }
@@ -51,28 +51,59 @@ function obtenerExtension() {
 async function ejecutarScriptSegunExtension(extension, file, id, kv, sc, os) {
   switch (extension) {
     case 'py':
-      return ejecutarPython(file + "." + extension, id, kv, sc, os);
+      return ejecutarScript("python", file + "." + extension, id, kv, sc, os)
     case 'cpp':
-      return ejecutarCPP(file + "." + extension, id, kv, sc, os);
+      return ejecutarScript("cpp", file + "." + extension, id, kv, sc, os)
     case 'c':
-      return ejecutarC(file + "." + extension, id, kv, sc, os);
+      return ejecutarScript("c", file + "." + extension, id, kv, sc, os)
     case 'sh':
-      return ejecutarSH(file + "." + extension, id, kv, sc, os);
+      return ejecutarScript("sh", file + "." + extension, id, kv, sc, os)
     default:
       console.log(`Extensión no soportada: ${extension}`);
-      await guardarEnOS(id, os, sc, `Extensión no soportada: ${extension}`)
+      await guardarEnOS(id, os, sc, "0s", `Extensión no soportada: ${extension}`)
       await keyValueChanges(id, kv, sc, "error")
       return Promise.resolve(null);
   }
 }
 
-async function ejecutarPython(scriptPath, id, kv, sc, os) {
+async function ejecutarScript(scriptType, scriptPath, id, kv, sc, os) {
   return new Promise(async (resolve, reject) => {
     try {
       // Registrar el tiempo de inicio
       const startTime = new Date();
 
-      const comando = `python3 ${path.join(destinationFolder, scriptPath)}`;
+      let comando;
+
+      switch (scriptType) {
+        case 'python':
+          comando = `python3 ${path.join(destinationFolder, scriptPath)}`;
+          break;
+        case 'cpp':
+          comando = `g++ ${path.join(destinationFolder, scriptPath)} -o ${path.join(destinationFolder, 'output')}`;
+          break;
+        case 'sh':
+          comando = `cd ${destinationFolder} && bash ${scriptPath} -o 'output'`;
+          break;
+        case 'c':
+          const ejecutablePath = path.join(destinationFolder, 'output');
+          const compileCommand = `gcc ${path.join(destinationFolder, scriptPath)} -o ${ejecutablePath}`;
+          const compileResult = await execAsync(compileCommand);
+
+          if (compileResult.stderr) {
+            console.error(`Error al compilar el código C: ${compileResult.stderr}`);
+            await guardarEnOS(id, os, sc, "0s", compileResult.stderr);
+            await keyValueChanges(id, kv, sc, "error");
+            reject(new Error(`Error al compilar el código C: ${compileResult.stderr}`));
+            return;
+          }
+
+          comando = ejecutablePath;
+          break;
+        default:
+          reject(new Error('Tipo de script no válido'));
+          return;
+      }
+
       const { stdout, stderr } = await execAsync(comando);
 
       // Registrar el tiempo de finalización
@@ -82,187 +113,25 @@ async function ejecutarPython(scriptPath, id, kv, sc, os) {
       const tiempoDeEjecucion = (endTime - startTime) / 1000;
 
       if (stderr) {
-        console.error(`Error al ejecutar el script de Python: ${stderr}`);
-        await guardarEnOS(id, os, sc, stderr, tiempoDeEjecucion + "s");
-        keyValueChanges(id, kv, sc, "error");
-        reject(new Error(`Error al ejecutar el script de Python: ${stderr}`));
-      } else {
-        console.log(`Script de Python ejecutado correctamente: ${stdout}`);
-
-        await guardarEnOS(id, os, sc, stdout, tiempoDeEjecucion + "s");
-        await keyValueChanges(id, kv, sc, "done");
-        await guardarEnKV(id, kv, sc, tiempoDeEjecucion + "s", stdout)
-        resolve(stdout);
-      }
-    } catch (error) {
-      console.error(`Error al ejecutar el script de Python: ${error.message}`);
-      await guardarEnOS(id, os, sc, error, tiempoDeEjecucion + "s");
-      await keyValueChanges(id, kv, sc, "error");
-      reject(error);
-    }
-  });
-}
-
-
-async function ejecutarCPP(scriptPath, id, kv, sc, os) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // Registrar el tiempo de inicio
-      const startTime = new Date();
-
-      const comando = `g++ ${path.join(destinationFolder, scriptPath)} -o ${path.join(destinationFolder, 'output')}`;
-      const compileResult = await execAsync(comando);
-
-      if (compileResult.stderr) {
-        console.error(`Error al compilar el código C++: ${compileResult.stderr}`);
-        await guardarEnOS(id, os, sc, compileResult.stderr);
+        console.error(`Error al ejecutar el script: ${stderr}`);
+        await guardarEnOS(id, os, sc, tiempoDeEjecucion + "s", stderr);
         await keyValueChanges(id, kv, sc, "error");
-        await reject(new Error(`Error al compilar el código C++: ${compileResult.stderr}`));
-        return;
-      }
-
-      const ejecutable = path.join(destinationFolder, 'output');
-      const { stdout, stderr } = await execAsync(ejecutable);
-
-      // Registrar el tiempo de finalización
-      const endTime = new Date();
-
-      // Calcular la diferencia de tiempo en milisegundos
-      const tiempoDeEjecucion = (endTime - startTime) / 1000;
-
-      if (stderr) {
-        console.error(`Error al ejecutar el código C++: ${stderr}`);
-        await guardarEnOS(id, os, sc, stderr, tiempoDeEjecucion + "s");
-        await keyValueChanges(id, kv, sc, "error");
-        reject(new Error(`Error al ejecutar el código C++: ${stderr}`));
+        reject(new Error(`Error al ejecutar el script: ${stderr}`));
       } else {
-        console.log(`Código C++ ejecutado correctamente: ${stdout}`);
+        console.log(`Script ejecutado correctamente: ${stdout}`);
 
-        await guardarEnOS(id, os, sc, stdout, tiempoDeEjecucion + "s");
+        await guardarEnOS(id, os, sc, tiempoDeEjecucion + "s", stdout);
         await keyValueChanges(id, kv, sc, "done");
-        await guardarEnKV(id, kv, sc, tiempoDeEjecucion + "s", stdout)
 
         resolve(stdout);
       }
     } catch (error) {
-      console.error(`Error al ejecutar el código C++: ${error.message}`);
-      await guardarEnOS(id, os, sc, error.message);
+      console.error(`Error al ejecutar el script: ${error.message}`);
+      await guardarEnOS(id, os, sc, "0s", error.message);
       await keyValueChanges(id, kv, sc, "error");
       reject(error);
     }
   });
-}
-
-async function ejecutarSH(scriptPath, id, kv, sc, os) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // Registrar el tiempo de inicio
-      const startTime = new Date();
-
-      const comando = `cd ${destinationFolder} && bash ${scriptPath} -o 'output'`;
-      const { stdout, stderr } = await execAsync(comando);
-
-      // Registrar el tiempo de finalización
-      const endTime = new Date();
-
-      // Calcular la diferencia de tiempo en milisegundos
-      const tiempoDeEjecucion = (endTime - startTime) / 1000;
-
-      if (stderr) {
-        console.error(`Error al ejecutar el sh: ${stderr}`);
-        await guardarEnOS(id, os, sc, stderr, tiempoDeEjecucion + "s");
-        await keyValueChanges(id, kv, sc, "error");
-        reject(new Error(`Error al ejecutar el sh: ${stderr}`));
-      } else {
-        console.log(`Código sh ejecutado correctamente: ${stdout}`);
-
-        await guardarEnOS(id, os, sc, stdout, tiempoDeEjecucion + "s");
-        await keyValueChanges(id, kv, sc, "done");
-        await guardarEnKV(id, kv, sc, tiempoDeEjecucion + "s", stdout)
-
-        resolve(stdout);
-      }
-    } catch (error) {
-      console.error(`Error al ejecutar el sh: ${error.message}`);
-      await guardarEnOS(id, os, sc, error.message);
-      await keyValueChanges(id, kv, sc, "error");
-      reject(error);
-    }
-  });
-}
-
-async function ejecutarC(scriptPath, id, kv, sc, os) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // Registrar el tiempo de inicio
-      const startTime = new Date();
-
-      const ejecutablePath = path.join(destinationFolder, 'output');
-      const compileCommand = `gcc ${path.join(destinationFolder, scriptPath)} -o ${ejecutablePath}`;
-      const compileResult = await execAsync(compileCommand);
-
-      if (compileResult.stderr) {
-        console.error(`Error al compilar el código C: ${compileResult.stderr}`);
-        await guardarEnOS(id, os, sc, compileResult.stderr);
-        await keyValueChanges(id, kv, sc, "error");
-        reject(new Error(`Error al compilar el código C: ${compileResult.stderr}`));
-        return;
-      }
-
-      const { stdout, stderr } = await execAsync(ejecutablePath);
-
-      // Registrar el tiempo de finalización
-      const endTime = new Date();
-
-      // Calcular la diferencia de tiempo en milisegundos
-      const tiempoDeEjecucion = (endTime - startTime) / 1000;
-
-      if (stderr) {
-        console.error(`Error al ejecutar el código C: ${stderr}`);
-        await guardarEnOS(id, os, sc, stderr, tiempoDeEjecucion + "s");
-        await keyValueChanges(id, kv, sc, "error");
-        reject(new Error(`Error al ejecutar el código C: ${stderr}`));
-      } else {
-        console.log(`Código C ejecutado correctamente: ${stdout}`);
-
-        await guardarEnOS(id, os, sc, stdout, tiempoDeEjecucion + "s");
-        await keyValueChanges(id, kv, sc, "done");
-        await guardarEnKV(id, kv, sc, tiempoDeEjecucion + "s", stdout)
-
-        resolve(stdout);
-        return stdout
-      }
-    } catch (error) {
-      console.error(`Error al ejecutar el código C: ${error.message}`);
-      await guardarEnOS(id, os, sc, error.message);
-      await keyValueChanges(id, kv, sc, "error");
-      reject(error);
-    }
-  });
-}
-
-function readableStreamFrom(data) {
-  return new ReadableStream({
-    start(controller) {
-      controller.enqueue(data);
-      controller.close();
-    },
-  });
-}
-
-async function fromReadableStream(ed, sc) {
-  result = ""
-  const reader = ed.getReader();
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) {
-      break;
-    }
-    if (value && value.length) {
-      result += sc.decode(value);
-    }
-  }
-  return result;
 }
 
 async function keyValueChanges(id, kv, sc, estadoParam) {
@@ -278,23 +147,6 @@ async function keyValueChanges(id, kv, sc, estadoParam) {
   }
 }
 
-async function guardarEnKV(id, kv, sc, tiempo, stdout) {
-  try {
-    let estado = await kv.get(id);
-
-    const dataJSON = JSON.parse(sc.decode(estado.value))
-    const finalState = JSON.stringify(dataJSON)
-    await kv.put(id, sc.encode(finalState))
-    console.log(finalState)
-    // Introducir un pequeño retraso antes de la siguiente operación
-    await new Promise(resolve => setTimeout(resolve, 100));
-
-
-  } catch (error) {
-    console.error(`Error al leer o guardar el archivo: ${error.message}`);
-  }
-}
-
 async function guardarEnOS(id, os, sc, tiempo, stdout) {
   try {
     let estado = await os.getBlob(id);
@@ -307,15 +159,15 @@ async function guardarEnOS(id, os, sc, tiempo, stdout) {
 
     const jsonData = JSON.stringify(dataJSON); // Convierto > JSON
     console.log('> Data stored in the OB Store: \n', jsonData);
-    
-    const bytes = new TextEncoder().encode(jsonData); // Convertir el JSON a Uint8Array
-      let data = new Uint8Array(bytes);
 
-      await os.putBlob({ name: id, description: "Contenido" }, data);
-      console.log(
-        `Se agregó una nueva entrada para ${id} (${finalState.length} bytes).\n`,
-        `El Object Storage ahora ocupa: ${status.size} bytes.\n`
-      );
+    const bytes = new TextEncoder().encode(jsonData); // Convertir el JSON a Uint8Array
+    let data = new Uint8Array(bytes);
+
+    await os.putBlob({ name: id, description: "Contenido" }, data);
+    console.log(
+      `Se agregó una nueva entrada para ${id} (${finalState.length} bytes).\n`,
+      `El Object Storage ahora ocupa: ${status.size} bytes.\n`
+    );
 
     // Introducir un pequeño retraso antes de la siguiente operación
     await new Promise(resolve => setTimeout(resolve, 100));
@@ -323,12 +175,6 @@ async function guardarEnOS(id, os, sc, tiempo, stdout) {
   } catch (error) {
     console.error(`Error al guardar en el Object Storage: ${error.message}`);
   }
-}
-
-
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function run() {
@@ -343,8 +189,6 @@ async function run() {
 
   groupName = 'group1'
   const sub = nc.subscribe('cola', { queue: groupName });
-  // const res = nc.('jobs_out', {queue: groupName });
-
 
   (async () => {
     for await (const m of sub) {
